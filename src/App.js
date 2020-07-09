284178bc10197fcdb1768f7fca5792706c313ca7
venegasgabby76 <gabbyvenegas76@yahoo.com>
2020-07-08 17:13:01 -0700

    forgot to import axios into my api folder. added my useEffect into my app file. seems to be working and an object array is showing when i inspect the console. going to start working on my episode test file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
+//importing my api call below
+import { fetchShow } from "./api/fetchShow";
+
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -14,19 +17,29 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
-  useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+  // useEffect(() => {
+  //   const fetchShow = () => {
+  //     axios
+  //       .get(
+  //         "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+  //       )
+  //       .then(res => {
+  //         setShow(res.data);
+  //         setSeasons(formatSeasons(res.data._embedded.episodes));
+  //       });
+  //   };
+  //   fetchShow();
+  // }, []);
+
+
+  useEffect (() => {
+    fetchShow()
+    .then(response => {
+      setShow(response.data);
+      setSeasons(formatSeasons(response.data._embedded.episodes))
+    })
   }, []);
+
 
   const handleSelect = e => {
     setSelectedSeason(e.value);
